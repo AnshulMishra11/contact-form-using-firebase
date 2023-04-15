907d285dac3ac89659b5a8721e8c330151e33a7d
AnshulMishra11 <111335247+AnshulMishra11@users.noreply.github.com>
2023-04-15 14:31:36 +0530

    Update mx.js

--- contact_form_firebase-master/mx.js
+++ contact_form_firebase-master/mx.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDqC9LhMHsc9YVMHGOO1z2pqmfMJBzt9-Y",
-    authDomain: "contact-a998d.firebaseapp.com",
-    databaseURL: "https://contact-a998d-default-rtdb.firebaseio.com",
-    projectId: "contact-a998d",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "contact-a998d.appspot.com",
     messagingSenderId: "547791479826",
-    appId: "1:547791479826:web:558df2eccbf30c7a6264cb"
+    appId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
